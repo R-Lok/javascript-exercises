eb79f0e47abbca3905cb145a7a6537982183046d
Ryan Lok <rlok.pc@gmail.com>
2022-05-09 14:42:46 +0800

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    let output = ""
 
-};
+    if (repeats >= 0) {
+
+    for (i = 1; i <= repeats; i++) {
+        output += string
+    }
+    return output
+} else {
+    return "ERROR"
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
