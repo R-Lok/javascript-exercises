99956b2859c7ba6cad414bd776fa2793ee13d547
Ryan Lok <rlok.pc@gmail.com>
2022-05-09 15:59:34 +0800

    Complete exercise 5

---
+++ 05_sumAll/planning.md
@@ -0,0 +1,14 @@
+
+create sum variable to contain the sum. Start at 0
+get all numbers between num1 and num2 via.. loop from num1 to num2
+for each loop, add current num to the sum variable
+return sum at tend of loop
+
+
+What if num2 is bigger than num1?
+Do a check at the start if num1 > num2
+If not, store num2 in temp2
+num2 = num1
+num1 = temp2
+
+then proceed with the loop
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
+    if (typeof(numOne) !== 'number' || typeof(numTwo) !== 'number' 
+        || (numOne || numTwo) < 0) {
+        return "ERROR"
+    }
+
+    
+    if (numOne > numTwo) {
+        let tempTwo = numTwo;
+        numTwo = numOne;
+        numOne = tempTwo;
+    }
+    
+    let sum = 0
+
+    for (i = numOne; i <= numTwo; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
