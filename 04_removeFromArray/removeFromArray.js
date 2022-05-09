d1e6b7adc110ecc197d42143acd0a9d202547ee1
Ryan Lok <rlok.pc@gmail.com>
2022-05-09 15:34:35 +0800

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
 
+    for (i = 1; i < arguments.length; i++) {
+
+        if (array.includes(arguments[i]) === false){
+            continue
+        } else {
+            let removeIndex = array.indexOf(arguments[i]);
+            array.splice(removeIndex, 1);
+        }
+       
+    }
+    return array
+
+    
 };
 
 // Do not edit below this line
