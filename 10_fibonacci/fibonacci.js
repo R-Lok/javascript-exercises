1c1afde12864153ba8d79c780571549ad8d8ff5e
Ryan Lok <rlok.pc@gmail.com>
2022-05-14 20:37:54 +0800

    Complete exercise 10, 11, 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number);
 
+    if (number > 0) {
+        let fiboArray = [1, 1];
+        for (i = 2; i < number; i++) {
+            fiboArray.push(fiboArray[i-1] + fiboArray[i-2]);
+        }
+        return fiboArray[number - 1];
+    } else if (number === 0) {
+        return 0;
+    } else {
+        return 'OOPS';
+    }
+    
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
 
+   peopleArray.forEach(person => {if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = new Date().getFullYear();
+    } else {
+        return;
+    }
+});
+
+    let peopleSorted = peopleArray.sort((personA, personB) => {
+        if (personA.yearOfDeath - personA.yearOfBirth > personB.yearOfDeath - personB.yearOfBirth) {
+            return -1
+        } else {
+            return 1
+        }
+    })
+    return peopleSorted[0];
 };
 
 // Do not edit below this line
